b2b251f56582196747e05b40dd385dd90dabff01
David Miller <David>
2012-03-21 19:22:37 +0000

    Thrift client works as a contextmanager

--- rpc/thrifty.py
+++ rpc/thrifty.py
@@ -32,20 +32,34 @@ class Client(clients.RpcProxy):
     """
     flavour = "Thrift"
 
-    def __init__(self, url, port, service, timeout=1):
+    def __init__(self, url, service, timeout=1):
         """
         We Allow either a URI we can parse a port number from,
         or a specific port keyword argument.
         """
         self._service = service
-        self.url = url
+        self.url, port = url.split(':')
+        self.port = int(port)
         self.timeout = timeout
-        self.port = port
-        self._client, self._transport = _clientmaker(service, url, port)
+        self._client, self._transport = _clientmaker(service, self.url, self.port)
 
     def __repr__(self):
         return "<{flavour} Client for {url}:{port}>".format(
             url=self.url, flavour=self.flavour, port=self.port)
+
+    def __enter__(self):
+        """
+        Open the transport and return the client for Thrift contextmanagers
+        """
+        self._transport.open()
+        return self._client
+
+    def __exit__(self, exc, type, stack):
+        """
+        Close the transport
+        """
+        self._transport.close()
+        return
 
 # @contextlib.contextmanager
 # def Client(service, host, port, framed=False):
