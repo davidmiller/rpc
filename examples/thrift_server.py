d1dd6bfc9cba6663453d7c75d8bc9f3e967f1ff5
David Miller <David>
2012-03-21 19:23:19 +0000

    Add example: Thrift server

---
+++ examples/thrift_server.py
@@ -0,0 +1,17 @@
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'gen-py')))
+
+from rpc import thrifty
+
+from service import Service
+
+class Handler(object):
+    def ping(self):
+        return "PONG!"
+
+
+if __name__ == '__main__':
+    server = thrifty.Server(port=4567, handler=Handler)
+
