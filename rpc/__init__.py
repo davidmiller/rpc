12b8208e8232a9c928dcf031d020f31ddfb2f92d
David Miller <david@deadpansincerity.com>
2012-03-28 15:16:15 +0100

    Include version as a module attribute

--- rpc/__init__.py
+++ rpc/__init__.py
@@ -4,8 +4,10 @@ rpc
 namespacing and packageability
 """
 from rpc import jsonrpc, thrifty
+from rpc._version import __version__
 
 __all__ = [
+    '__version__',
     'jsonrpc',
     'thrifty'
     ]
